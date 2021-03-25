63233e33cbce9c68356213c5f1c72bd6adbd6ff9
Tomas <tomizechsterson@gmail.com>
2021-03-24 20:36:43 -0500

    Add ability to delete account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to characters_url, alert: "Account successfully deleted"
+  end
+
   private
 
   def user_params
