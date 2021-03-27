0a77555a79d616453b49fae56c5fa83c9fbccd36
Tomas <tomizechsterson@gmail.com>
2021-03-27 13:34:13 -0500

    Redirect users to where they were going after signing in
    Also add tests to cover this

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,24 +18,46 @@ RSpec.describe "/users" do
   }}
 
   describe "GET /index" do
-    it "renders a successful response" do
-      User.create! valid_attributes
-      sign_in
+    context "if signed in" do
+      it "renders a successful response" do
+        User.create! valid_attributes
+        sign_in_with_valid_user
 
-      get users_url
+        get users_url
 
-      expect(response).to be_successful
+        expect(response).to be_successful
+      end
+    end
+
+    context "if not signed in" do
+      it "redirects to sign in" do
+        get users_url
+
+        expect_redirect_to_sign_in(response)
+      end
     end
   end
 
   describe "GET /show" do
-    it "renders a successful response" do
-      user = User.create! valid_attributes
-      sign_in
+    context "if signed in" do
+      it "renders a successful response" do
+        user = User.create! valid_attributes
+        sign_in_with_valid_user
 
-      get user_url(user)
+        get user_url(user)
 
-      expect(response).to be_successful
+        expect(response).to be_successful
+      end
+    end
+
+    context "if not signed in" do
+      it "redirects to sign in" do
+        user = User.create! valid_attributes
+
+        get user_url(user)
+
+        expect_redirect_to_sign_in(response)
+      end
     end
   end
 
@@ -47,13 +69,25 @@ RSpec.describe "/users" do
   end
 
   describe "GET /edit" do
-    it "renders a successful response" do
-      user = User.create! valid_attributes
-      sign_in
+    context "if signed in" do
+      it "renders a successful response" do
+        user = User.create! valid_attributes
+        sign_in_with_valid_user
 
-      get edit_user_url(user)
+        get edit_user_url(user)
 
-      expect(response).to be_successful
+        expect(response).to be_successful
+      end
+    end
+
+    context "if not signed in" do
+      it "redirects to sign in" do
+        user = User.create! valid_attributes
+
+        get edit_user_url(user)
+
+        expect_redirect_to_sign_in(response)
+      end
     end
   end
 
@@ -102,31 +136,43 @@ RSpec.describe "/users" do
     context "with valid parameters" do
       let(:new_attributes) { { name: "updated name" } }
 
-      it "updates the requested user" do
-        user = User.create! valid_attributes
-        sign_in
+      context "when signed in" do
+        it "updates the requested user" do
+          user = User.create! valid_attributes
+          sign_in_with_valid_user
 
-        patch user_url(user), params: { user: new_attributes }
-        user.reload
+          patch user_url(user), params: { user: new_attributes }
+          user.reload
 
-        expect(user.name).to eq("updated name")
+          expect(user.name).to eq("updated name")
+        end
+
+        it "redirects to the user" do
+          user = User.create! valid_attributes
+          sign_in_with_valid_user
+          patch user_url(user), params: { user: new_attributes }
+
+          user.reload
+
+          expect(response).to redirect_to(user_url(user))
+        end
       end
 
-      it "redirects to the user" do
-        user = User.create! valid_attributes
-        sign_in
-        patch user_url(user), params: { user: new_attributes }
+      context "when not signed in" do
+        it "redirects to sign in" do
+          user = User.create! valid_attributes
 
-        user.reload
+          patch user_url(user), params: { user: new_attributes }
 
-        expect(response).to redirect_to(user_url(user))
+          expect_redirect_to_sign_in(response)
+        end
       end
     end
 
     context "with invalid parameters" do
       before do
         user = User.create! valid_attributes
-        sign_in
+        sign_in_with_valid_user
         patch user_url(user), params: { user: invalid_attributes }
       end
 
@@ -134,37 +180,59 @@ RSpec.describe "/users" do
         expect(response).to be_successful
       end
 
-      it "includes 'Name can't be blank' in the response body" do
-        expect(response.body).to include("Name can&#39;t be blank")
+      it "includes expected errors in the response body" do
+        expect(response.body).to include(
+                                   "Password confirmation doesn&#39;t match Password",
+                                   "Name can&#39;t be blank",
+                                   "Email is invalid",
+                                   "Password is too short (minimum is 10 characters)",
+                                   "Username is invalid"
+                                 )
       end
     end
   end
 
   describe "DELETE /destroy" do
-    it "destroys the requested user" do
-      user = User.create! valid_attributes
-      sign_in
+    context "when signed in" do
+      it "destroys the requested user" do
+        user = User.create! valid_attributes
+        sign_in_with_valid_user
 
-      expect {
+        expect {
+          delete user_url(user)
+        }.to change(User, :count).by(-1)
+      end
+
+      it "redirects to the users list" do
+        user = User.create! valid_attributes
+        sign_in_with_valid_user
+
         delete user_url(user)
-      }.to change(User, :count).by(-1)
+
+        expect(response).to redirect_to(characters_url)
+      end
     end
 
-    it "redirects to the users list" do
-      user = User.create! valid_attributes
-      sign_in
+    context "when not signed in" do
+      it "redirects to sign in" do
+        user = User.create! valid_attributes
 
-      delete user_url(user)
+        delete user_url(user)
 
-      expect(response).to redirect_to(characters_url)
+        expect_redirect_to_sign_in(response)
+      end
     end
   end
 
   private
 
-  def sign_in
+  def sign_in_with_valid_user
     post session_url, params: {
       email_or_username: valid_attributes[:email], password: valid_attributes[:password]
     }
+  end
+
+  def expect_redirect_to_sign_in(response)
+    expect(response).to redirect_to(new_session_url)
   end
 end
