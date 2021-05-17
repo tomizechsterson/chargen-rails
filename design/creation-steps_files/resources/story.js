8e3113e8f425137a7a6e2907bd567ea310231bbb
Tomas <tomizechsterson@gmail.com>
2021-05-16 20:58:27 -0500

    Add design folder
    Also include first sketch of character creation

---
+++ design/creation-steps_files/resources/story.js
@@ -0,0 +1,333 @@
+var story = {
+ "pages": [
+  {
+   "image": "CharacterEdit.png",
+   "image2x": "CharacterEdit@2x.png",
+   "width": 841,
+   "links": [{
+    "rect": [
+     461,
+     183,
+     579,
+     208
+    ],
+    "page": 12
+   }],
+   "title": "CharacterEdit",
+   "height": 625
+  },
+  {
+   "image": "CreationAdd7Dice.png",
+   "image2x": "CreationAdd7Dice@2x.png",
+   "width": 841,
+   "links": [{
+    "rect": [
+     541,
+     434,
+     624,
+     459
+    ],
+    "page": 9
+   }],
+   "title": "CreationAdd7Dice",
+   "height": 625
+  },
+  {
+   "image": "CreationAlignment.png",
+   "image2x": "CreationAlignment@2x.png",
+   "width": 841,
+   "links": [
+    {
+     "rect": [
+      532,
+      400,
+      615,
+      425
+     ],
+     "page": 8
+    },
+    {
+     "rect": [
+      445,
+      400,
+      528,
+      425
+     ],
+     "page": 4
+    }
+   ],
+   "title": "CreationAlignment",
+   "height": 625
+  },
+  {
+   "image": "CreationAssignment.png",
+   "image2x": "CreationAssignment@2x.png",
+   "width": 841,
+   "links": [{
+    "rect": [
+     541,
+     434,
+     624,
+     459
+    ],
+    "page": 9
+   }],
+   "title": "CreationAssignment",
+   "height": 625
+  },
+  {
+   "image": "CreationClass.png",
+   "image2x": "CreationClass@2x.png",
+   "width": 841,
+   "links": [
+    {
+     "rect": [
+      534,
+      400,
+      617,
+      425
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      445,
+      400,
+      528,
+      425
+     ],
+     "page": 7
+    }
+   ],
+   "title": "CreationClass",
+   "height": 625
+  },
+  {
+   "image": "CreationDblAssignment.png",
+   "image2x": "CreationDblAssignment@2x.png",
+   "width": 841,
+   "links": [{
+    "rect": [
+     541,
+     434,
+     624,
+     459
+    ],
+    "page": 9
+   }],
+   "title": "CreationDblAssignment",
+   "height": 625
+  },
+  {
+   "image": "CreationFinal.png",
+   "image2x": "CreationFinal@2x.png",
+   "width": 841,
+   "links": [{
+    "rect": [
+     445,
+     400,
+     528,
+     425
+    ],
+    "page": 8
+   }],
+   "title": "CreationFinal",
+   "height": 625
+  },
+  {
+   "image": "CreationHeightWeight.png",
+   "image2x": "CreationHeightWeight@2x.png",
+   "width": 841,
+   "links": [
+    {
+     "rect": [
+      533,
+      400,
+      616,
+      425
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      445,
+      400,
+      528,
+      425
+     ],
+     "page": 9
+    }
+   ],
+   "title": "CreationHeightWeight",
+   "height": 625
+  },
+  {
+   "image": "CreationHitPoints.png",
+   "image2x": "CreationHitPoints@2x.png",
+   "width": 841,
+   "links": [
+    {
+     "rect": [
+      531,
+      400,
+      614,
+      425
+     ],
+     "page": 6
+    },
+    {
+     "rect": [
+      445,
+      400,
+      528,
+      425
+     ],
+     "page": 2
+    }
+   ],
+   "title": "CreationHitPoints",
+   "height": 625
+  },
+  {
+   "image": "CreationRace.png",
+   "image2x": "CreationRace@2x.png",
+   "width": 841,
+   "links": [
+    {
+     "rect": [
+      537,
+      400,
+      620,
+      425
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      445,
+      400,
+      528,
+      425
+     ],
+     "page": 12
+    }
+   ],
+   "title": "CreationRace",
+   "height": 625
+  },
+  {
+   "image": "CreationRollFour.png",
+   "image2x": "CreationRollFour@2x.png",
+   "width": 841,
+   "links": [{
+    "rect": [
+     541,
+     434,
+     624,
+     459
+    ],
+    "page": 9
+   }],
+   "title": "CreationRollFour",
+   "height": 625
+  },
+  {
+   "image": "CreationRollOnce.png",
+   "image2x": "CreationRollOnce@2x.png",
+   "width": 841,
+   "links": [{
+    "rect": [
+     541,
+     434,
+     624,
+     459
+    ],
+    "page": 9
+   }],
+   "title": "CreationRollOnce",
+   "height": 625
+  },
+  {
+   "image": "CreationRollStats.png",
+   "image2x": "CreationRollStats@2x.png",
+   "width": 841,
+   "links": [
+    {
+     "rect": [
+      293,
+      223,
+      420,
+      244
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      293,
+      245,
+      420,
+      265
+     ],
+     "page": 13
+    },
+    {
+     "rect": [
+      293,
+      265,
+      420,
+      285
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      293,
+      285,
+      420,
+      305
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      293,
+      305,
+      420,
+      325
+     ],
+     "page": 10
+    },
+    {
+     "rect": [
+      293,
+      325,
+      420,
+      345
+     ],
+     "page": 1
+    }
+   ],
+   "title": "CreationRollStats",
+   "height": 625
+  },
+  {
+   "image": "CreationRollTwice.png",
+   "image2x": "CreationRollTwice@2x.png",
+   "width": 841,
+   "links": [{
+    "rect": [
+     541,
+     434,
+     624,
+     459
+    ],
+    "page": 9
+   }],
+   "title": "CreationRollTwice",
+   "height": 625
+  }
+ ],
+ "resolutions": [2],
+ "title": "creation-steps",
+ "highlightLinks": true
+}
